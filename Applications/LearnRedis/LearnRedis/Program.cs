4f80a3e120ac1b086833bf4afece54675fa1e2d4
Charles Gathu <charles.gathu@live.com>
2019-09-08 00:34:01 +0200

    Initial checkin

---
+++ Applications/LearnRedis/LearnRedis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
